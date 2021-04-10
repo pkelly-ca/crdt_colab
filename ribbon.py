854d5b65bf3f0656c2a196e77ff691f07678970d
pkelly-ca <62775774+pkelly-ca@users.noreply.github.com>
2021-04-10 14:23:48 -0500

    added LA

--- ribbon.py
+++ ribbon.py
@@ -416,6 +416,25 @@ def runKY(path,date,state,keys):
   # Return ribbon
   return df_st
 
+def runLA(path,date,state,keys):
+  # Read state file(s)
+  num_files = 1 ### Edit this to equal the number of files in the repo
+  df = {}
+  for i in range(1,num_files+1):
+    df[i] = st_csv(i,path,date,state)
+    df[i]=df[i].drop('Unnamed: 0',axis=1)
+    display(df[i])
+  df = df[1].set_index(['LDH_Region','Race'])
+  df = df.sum(level=1)
+  df.loc['Total']=df.sum()
+  df = df.reset_index()
+  # Pre-processing
+  # Common processing
+  df_st = state_common(df,keys,state)
+  # Custom Mapping
+  # Return ribbon
+  return df_st
+
 def template(path,date,state,keys):
   # Read state file(s)
   num_files = 2 ### Edit this to equal the number of files in the repo
@@ -440,7 +459,7 @@ key = load_state_keys('crdt_key.csv')
 #          "NY","OR","PA","RI","SD","TN","TX","UT","VA","VT",
 #          "WA","WI","WY"]
 
-states_all = ["AK","AL","AR","CA","CO","CT","DC","DE","FL","GA","GU","HI","ID","IL","IN"]
+states_all = ["AK","AL","AR","CA","CO","CT","DC","DE","FL","GA","GU","HI","ID","IL","IN","KY"]
 #states = ["FL"]
 date_str = datetime.datetime.now().strftime("%Y%m%d") 
 

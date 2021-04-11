0e6462b20dc11e3ee262b8fb71ae2479f52eec74
pkelly-ca <62775774+pkelly-ca@users.noreply.github.com>
2021-04-11 17:10:19 -0500

    added ND

--- ribbon.py
+++ ribbon.py
@@ -648,6 +648,24 @@ def runNC(path,date,state,keys):
   # Return ribbon
   return df_st
 
+def runND(path,date,state,keys):
+  # Read state file(s)
+  num_files = 1 ### Edit this to equal the number of files in the repo
+  df = {}
+  for i in range(1,num_files+1):
+    df[i] = st_csv(i,path,date,state)
+    df[i]=df[i].drop(['Unnamed: 0'],axis=1)
+    display(df[i])
+  # Pre-processing
+  df=df[1].set_index('Category')
+  df.loc['Total']=df.sum()
+  df=df.reset_index()
+  # Common processing
+  df_st = state_common(df,keys,state)
+  # Custom Mapping
+  # Return ribbon
+  return df_st
+
 def template(path,date,state,keys):
   # Read state file(s)
   num_files = 2 ### Edit this to equal the number of files in the repo

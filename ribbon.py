49465d327177d2cbab574eb2db21cfbd18840a13
pkelly-ca <62775774+pkelly-ca@users.noreply.github.com>
2021-04-14 07:28:01 -0500

    added SD

--- ribbon.py
+++ ribbon.py
@@ -865,6 +865,26 @@ def runRI(path,date,state,keys):
   # Return ribbon
   return df_st
 
+def runSD(path,date,state,keys):
+  # Read state file(s)
+  num_files = 1 ### Edit this to equal the number of files in the repo
+  df = {}
+  for i in range(1,num_files+1):
+    df[i] = st_csv(i,path,date,state)
+    df[i]=df[i].drop(['Unnamed: 0'],axis=1)
+    display(df[i])
+  # Pre-processing
+  df[1].columns = ['Category','Cases','Deaths','Hospitalizations']
+  df = df[1].set_index('Category')
+  df.loc['Total']=df.sum()
+  df.loc['NH']=df.loc['Total']-df.loc['Hispanic']-df.loc['Unknown, Non-Hispanic']
+  df = df.reset_index()
+  # Common processing
+  df_st = state_common(df,keys,state)
+  # Custom Mapping
+  # Return ribbon
+  return df_st
+
 def template(path,date,state,keys):
   # Read state file(s)
   num_files = 2 ### Edit this to equal the number of files in the repo

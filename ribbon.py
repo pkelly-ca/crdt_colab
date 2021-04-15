07baa7ff72daadbf4b79b11a64a53655bc09b0da
pkelly-ca <62775774+pkelly-ca@users.noreply.github.com>
2021-04-14 19:37:53 -0500

    added WA

--- ribbon.py
+++ ribbon.py
@@ -982,6 +982,26 @@ def runVT(path,date,state,keys):
   # Return ribbon
   return df_st
 
+def runWA(path,date,state,keys):
+  # Read state file(s)
+  num_files = 3 ### Edit this to equal the number of files in the repo
+  df = {}
+  for i in range(1,num_files+1):
+    df[i] = st_csv(i,path,date,state)
+    df[i]=df[i].drop(['Unnamed: 0'],axis=1)
+    df[i]['Category']=df[i]['Category'].replace(' \([^\)]*\)','',regex=True)
+    df[i].loc[0,'Category']='Total'
+    display(df[i])
+  # Pre-processing
+  df = df[1].merge(df[2],how='outer',on='Category').merge(df[3],how='outer',on='Category').set_index('Category')
+  df.loc['NH']=df.loc['Total with Race/Ethnicity Available']-df.loc['Hispanic']
+  df = df.reset_index()
+  # Common processing
+  df_st = state_common(df,keys,state)
+  # Custom Mapping
+  # Return ribbon
+  return df_st
+
 def template(path,date,state,keys):
   # Read state file(s)
   num_files = 2 ### Edit this to equal the number of files in the repo

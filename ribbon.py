0a31c9c5dffacb8dad004f7b8e76139a7c9757b8
pkelly-ca <62775774+pkelly-ca@users.noreply.github.com>
2021-04-14 20:34:42 -0500

    added WY

--- ribbon.py
+++ ribbon.py
@@ -1037,6 +1037,35 @@ def runWI(path,date,state,keys):
   # Return ribbon
   return df_st
 
+def runWY(path,date,state,keys):
+  # Read state file(s)
+  num_files = 5 ### Edit this to equal the number of files in the repo
+  df = {}
+  for i in range(1,num_files+1):
+    df[i] = st_csv(i,path,date,state)
+    df[i]=df[i].drop(['Unnamed: 0'],axis=1)
+    display(df[i])
+  # Pre-processing
+  tots=[df[1].iloc[0,3],int(df[2].iloc[0,2]),df[3].iloc[0,1]]
+  display(tots)
+  for i in range(4,6):
+    df[i]=df[i].drop('%',axis=1)
+  df[4].columns=['Category','Cases']
+  df[5].columns=['Category','Deaths']
+  df[5]['Deaths']=df[5]['Deaths'].astype('int')
+  df = df[4].merge(df[5],how='outer',on='Category')
+  df.loc[len(df.index)]=['Total',tots[0]+tots[1],tots[2]]
+  df = df.set_index('Category')
+  df.loc['Known']=df.iloc[0:7].sum()
+  df.loc['NH']=df.loc['Known']-df.loc['Hispanic']
+  df.loc['Unk (calc)']=df.loc['Unknown']+tots[1]
+  df = df.reset_index()
+  # Common processing
+  df_st = state_common(df,keys,state)
+  # Custom Mapping
+  # Return ribbon
+  return df_st
+
 def template(path,date,state,keys):
   # Read state file(s)
   num_files = 2 ### Edit this to equal the number of files in the repo
